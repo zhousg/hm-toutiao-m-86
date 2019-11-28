f20f5b2a8b1ebed3f10af7cfcee9e8743453f559
zhousg <zhoushugang@itcast.cn>
2019-11-28 10:07:17 +0800

    项目初始化

---
+++ src/views/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class='container'>
+    <!-- 顶部导航 -->
+
+    <!-- 底部tab栏 -->
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'></style>
