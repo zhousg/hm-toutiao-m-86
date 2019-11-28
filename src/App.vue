3df670823119058b2e26ba9bbb9c81e00c8113c6
zhousg <zhoushugang@itcast.cn>
2019-11-28 10:20:52 +0800

    使用vant

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    App  <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 组件库  插件形态
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
